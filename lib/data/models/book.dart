f1ec8620285a9e95958f3f3b7a0094f48ff8b5be
Quang <trandinhquang2005@gmail.com>
2026-04-14 22:39:01 +0700

    home

---
+++ lib/data/models/book.dart
@@ -0,0 +1,31 @@
+class Book {
+  final int id;
+  final String title;
+  final String status;
+  final String isbn;
+  final String categoryName;
+  final String locationZone;
+  final double marketPrice;
+
+  Book({
+    required this.id,
+    required this.title,
+    required this.status,
+    required this.isbn,
+    required this.categoryName,
+    required this.locationZone,
+    required this.marketPrice,
+  });
+
+  factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+      id: json['book_id'] ?? 0,
+      title: json['title'] ?? 'Unknown Book',
+      status: json['status'] ?? 'N/A',
+      isbn: json['isbn'] ?? '',
+      categoryName: json['category'] != null ? json['category']['name'] : 'Thể loại khác',
+      locationZone: json['location'] != null ? json['location']['zone_name'] ?? '' : '',
+      marketPrice: json['market_price'] != null ? double.tryParse(json['market_price'].toString()) ?? 0.0 : 0.0,
+    );
+  }
+}
